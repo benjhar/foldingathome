c146d2f516b8cd92050a42656b99d488475a64a3
thenamesweretakenalready <43702423+thenamesweretakenalready@users.noreply.github.com>
2019-07-12 18:52:53 +0100

    Yes

---
+++ setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+setup(
+  name = 'foldingathome',
+  packages = ['foldingathome'],
+  version = '0.1',
+  license='GNU GENERAL PUBLIC LICENSE VERSION 3',
+  description = 'A Python wrapper for the Folding@Home API.',
+  author = 'leet_hakker',
+  author_email = 'leet_haker@cyber-wizard.com',
+  url = 'https://github.com/thenamesweretakenalready/foldingathome/',
+  download_url = 'https://github.com/thenamesweretakenalready/foldingathome/archive/v0.1.tar.gz',
+  keywords = ['API', 'Python', 'Folding@Home'],
+  install_requires=[
+          'requests',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
